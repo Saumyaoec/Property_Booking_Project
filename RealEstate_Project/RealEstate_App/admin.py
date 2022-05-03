ccff94d30df7959543469c89534ae0fafde962e3
prabhat <prabhat.webkrone@gmail.com>
2022-05-03 10:53:05 +0530

    Taggit,countryfields and uuid fields added

--- RealEstate_Project/RealEstate_App/admin.py
+++ RealEstate_Project/RealEstate_App/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Property, Room, Address
 # Register your models here.
+
+admin.site.register(Property)
+admin.site.register(Room)
+admin.site.register(Address)
--- RealEstate_Project/RealEstate_Project/settings.py
+++ RealEstate_Project/RealEstate_Project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'RealEstate_App'
+    'RealEstate_App',
+    'taggit',
+    'django_countries',
 ]
 
 MIDDLEWARE = [
